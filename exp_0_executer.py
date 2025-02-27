7acd4a13f036971cb77bdf59b7c374ea9acb815e
Omer Faruk Yildiran <omer.yildiran@protonmail.ch>
2025-02-27 03:14:55 -0500

    organized and visual durat estimate

---
+++ exp_0_executer.py
@@ -0,0 +1,12 @@
+ExpTesting = False
+ExpTraining= False
+expName = 'visualDurEstimate'
+
+# 1 - Inititate and welcome participants
+exec(open("exp_1_openScreen.py").read())
+
+# 2 - Import components
+exec(open("exp_2_importComponents.py").read()) 
+
+# 3 - Run trial and response Loop
+exec(open("exp_3_trialRoutine.py").read())
