bcbfa83ef4f3d2b651f308d2858db61145b64b43
Omer Faruk Yildiran <omer.yildiran@protonmail.ch>
2025-08-31 15:46:37 +0300

    model comparison update

--- loadResults.py
+++ loadResults.py
@@ -1,6 +1,18 @@
 import os
 import json
-def loadFitResults(dataName, modelName="lognorm"):
+def loadFitResults(fitter,dataName, modelName="lognorm"):
+        
+    if fitter.sharedLambda:
+        modelName += "_LapseFix"
+    else:
+        modelName += "_LapseFree"
+    
+    if fitter.freeP_c:
+        modelName += "_contextualPrior"
+    else:
+        modelName += "_sharedPrior"
+    print(f"Looking for saved fit: {dataName} with model {modelName}")
+
     participantID = dataName.split('_')[0]
     filepath = os.path.join("model_fits", participantID, f"{participantID}_{modelName}_fit.json")
     try:
